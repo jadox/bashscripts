f2c9170b38c78cf048a148411245ef363d7f00f9
Jadox <cristi@Cristian-MacBook.local>
2015-01-20 12:09:05 +0100

    Added log file and temporary file for the found entries

--- main.sh
+++ main.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-echo "Hello there!"
-say "Nothing else to do!"
-osascript<<EOF
-try
-tell application id "com.apple.ScreenSaver.Engine" to launch
-end try
-EOF
+echo "Looking for new entries..."
+date>>log.txt
+sqlite3 ~/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV* 'select LSQuarantineDataURLString from LSQuarantineEvent'>tmp.txt
+entries=$(wc -l <tmp.txt);
+echo $entries;
 
-say "Nothing"
+if [ $entries == 0 ]
+then say "Nothing found!"
+else say "Found stuff on database!" echo "$entries"
+fi
+
+#say "Nothing else to do!"
+#osascript<<EOF
+#try
+#tell application id "com.apple.ScreenSaver.Engine" to launch
+#end try
+#EOF
+
+
+say "Done!"
